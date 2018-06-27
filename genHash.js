5fe76adba147c19223b4191a15213714d1eee90c
Krister Viirsaar <krister.viirsaar@gmail.com>
2018-06-27 19:09:06 +0300

    Generate QR codes

--- genHash.js
+++ genHash.js
@@ -1,22 +1,41 @@
 var crypto = window.crypto || window.msCrypto;
 
+try {
+    Function("() => {};")
+}
+catch (e) {
+    updateStatus('Your browser is too old!')
+}
+
 var conf = {
     keyword: 'ourmotherfuckingsecretcode',
     eventname: 'tallinn20180611',
-    tickets: 6,
+    tickets: 32,
+    claimurl: 'https://fervent-panini-45fa4b.netlify.com?claim='
 }
 
 async function start() {
+    updateStatus('Generating QR codes...')
     var generatedHashes = []
+    const parentDiv = document.querySelector('#qrcodes')
     for (var i = 0; i < conf.tickets; i++) {
+        updateCount(i)
         var code = conf.keyword + conf.eventname + i
         var hash = await getHash(code)
         generatedHashes.push(hash)
-    }
 
-    generatedHashes.forEach(hash => {
-        document.querySelector('ul').innerHTML += `<p>${hash}</p>`
-    })
+        const element = QRCode.generateSVG(conf.claimurl + hash, {
+            fillcolor: "#fff",
+            textcolor: "#000",
+            margin: 4,
+            modulesize: 8
+        })
+        parentDiv.appendChild(element)
+        document.body.appendChild(parentDiv)
+    }
+    updateCount(conf.tickets)
+    updateStatus('Done')
+
 }
 
 async function getHash(value, algorithm = 'SHA-1') {
@@ -24,6 +43,14 @@ async function getHash(value, algorithm = 'SHA-1') {
     let hashBuffer = await crypto.subtle.digest(algorithm, msgBuffer);
     let hashArray = Array.from(new Uint8Array(hashBuffer));
     return hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+}
+
+function updateStatus(msg, count) {
+    document.querySelector('.qrstatus').innerHTML = msg
+}
+
+function updateCount(count) {
+    document.querySelector('.qrcount').innerHTML = `(${count}/${conf.tickets})`
 }
 
 start()
